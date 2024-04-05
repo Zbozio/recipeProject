dd58a392437f120c3c1d915150655ff780fe6e3f
Bozio <karollos.bozek@gmail.com>
2024-04-05 12:04:05 +0200

    Recipes added

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent {
 
   recipes:Recipe[]=[
+    new Recipe('A Test Recipe',"This is simply a test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"),
     new Recipe('A Test Recipe',"This is simply a test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
+
   ];
 
 }
