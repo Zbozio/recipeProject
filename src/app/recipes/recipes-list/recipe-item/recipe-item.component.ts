757c77ad52c8f7471420e885098de88e2d0d95cb
Bozio <karollos.bozek@gmail.com>
2024-04-21 19:06:48 +0200

    working on recipe site

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,5 +7,5 @@ import { Recipe } from '../../recipe.model';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
-
+ @Input() recipe:Recipe;
 }
