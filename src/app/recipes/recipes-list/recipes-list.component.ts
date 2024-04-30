06d75fde708af5b3e1068fee0afae010ad37ae89
Bozio <karollos.bozek@gmail.com>
2024-04-30 19:09:58 +0200

    idk

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,7 +10,7 @@ export class RecipesListComponent {
 @Output() recipeWasSelected=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
     new Recipe('A Test Recipe',"This is simply a test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"),
-    new Recipe('A Test Recipe',"This is simply a test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
+    new Recipe('A Test Second Recipe',"This is simply a test","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
 
   ];
 
